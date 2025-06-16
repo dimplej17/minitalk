1cf6c464f1ae6b8c306cabb82ff4b0208fbe7fb5
Dimple Janardhan <dimplejanardhan@Dimples-Air.fritz.box>
2025-06-16 08:57:06 +0200

    error handling & norm incomplete

--- minitalk.h
+++ minitalk.h
@@ -1,5 +1,7 @@
 #ifndef MINITALK_H
 # define MINITALK_H
+#include "libft_42/libft.h"
+#include "ft_printf_42/ft_printf.h"
 #include <unistd.h> // getpid()
 #include <signal.h> // sigaction(), kill()
 
--- server.c
+++ server.c
@@ -7,12 +7,52 @@
 // Reconstruct chars with bit shifting like:char |= (1 << bit_index);  // if bit is 1
 // Print the character once all 8 bits are received.
 
+// to check git logs: git log | grep -i "commit" | wc -l
+
 #include "minitalk.h"
 
-int main(void)
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+static void	signal_handler(int sig, siginfo_t *info, void *context)
 {
+	static int				bit_index = 0;
+	static unsigned char	char_accumulator = 0;
+
+	(void)context;
+	(void)info;
+	char_accumulator <<= 1;
+
+	if (sig == SIGUSR1)
+		char_accumulator |= 0;
+	else if (sig == SIGUSR2)
+		char_accumulator |= 1;
+
+	bit_index++;
+
+	if (bit_index == 8)
+	{
+		write(1, &char_accumulator, 1);
+		bit_index = 0;
+		char_accumulator = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
     pid_t pid_server;
     pid_server = getpid();
-    ft_printf("PID of server: %i\n", pid_server);
-    return (0);
+    ft_printf("Server PID: %d\n", pid_server);
+
+	sa.sa_sigaction = &signal_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+
+	while (1)
+		pause();
 }
